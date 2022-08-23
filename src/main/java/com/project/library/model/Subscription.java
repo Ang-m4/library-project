aa05c4ff0ee096e5382fc3fc94740f02bf7448ef
m4la <anfegima@hotmail.com>
2022-08-23 09:56:17 -0500

    JsonIgnore added on readers

--- src/main/java/com/project/library/model/Subscription.java
+++ src/main/java/com/project/library/model/Subscription.java
@@ -10,6 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "subscriptions")
 public class Subscription{
@@ -18,7 +20,8 @@ public class Subscription{
     @GeneratedValue
     @Column(name = "subscription_id")
     private Long id;
-
+    
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "reader_id")
     private Reader reader;
