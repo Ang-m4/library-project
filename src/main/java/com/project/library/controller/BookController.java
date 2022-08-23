5e677a45cb0bf5fab8fcf9725d369c72c95a6fd4
m4la <anfegima@hotmail.com>
2022-08-23 09:01:28 -0500

    Book CRUD endpoints completed

--- src/main/java/com/project/library/controller/BookController.java
+++ src/main/java/com/project/library/controller/BookController.java
@@ -2,12 +2,17 @@ package com.project.library.controller;
 import com.project.library.Db.BookRepository;
 import com.project.library.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/book")
@@ -21,9 +26,27 @@ public class BookController {
         return (List<Book>)bookRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Book getBook(@PathVariable Long id) {
-        return bookRepository.findById(id).get();
+    @GetMapping("/{isbn}")
+    public Book getBook(@PathVariable Long isbn) {
+        return bookRepository.findById(isbn).get();
     }
     
+    @PostMapping("/add")
+    public Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("/{isbn}/update")
+    public Book updateBook(@PathVariable Long isbn, @RequestBody Book book) {
+        
+        book.setIsbn(isbn);
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/{isbn}/delete")
+    public void deleteBook(@PathVariable Long isbn) {
+        bookRepository.deleteById(isbn);
+
+    }
+
 }
